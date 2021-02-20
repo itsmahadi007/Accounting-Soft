046475c96d34ccbe246413b9018ddff34810d162
Mahadi Hassan <itmahadi@gmail.com>
2021-02-20 17:49:40 +0600

    updates

--- Accounting Soft/log_reg_page.xaml.cs
+++ Accounting Soft/log_reg_page.xaml.cs
@@ -40,5 +40,64 @@ namespace Accounting_Soft
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
         }
+
+        private void username_textbox_Gotfocus(object sender, RoutedEventArgs e)
+        {
+            if (username_textbox.Text == "User Name ")
+            {
+                username_textbox.Text = "";
+            }
+        }
+
+        private void password_passbox_Gotfocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (password_passbox.Password == "Password ")
+            {
+                password_passbox.Password = "";
+            }
+        }
+
+        private void new_username_textbox_Getfocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if(new_username_textbox.Text == "Name")
+            {
+                new_username_textbox.Text = "";
+            }
+        }
+
+        private void new_email_textbox_Getfocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (new_email_textbox.Text == "Email")
+            {
+                new_email_textbox.Text = "";
+            }
+        }
+
+        private void new_number_textbox_Getfocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (new_number_textbox.Text == "Number")
+            {
+                new_number_textbox.Text = "";
+            }
+        }
+
+        private void new_password_textbox_Getfocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if(new_password_textbox.Text == "Enter Password ")
+            {
+                new_password_textbox.Text = "";
+            }
+        }
+
+        private void new_re_password_textbox_Getfocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if(new_re_password_textbox.Text == "Again Enter Password ")
+            {
+                new_re_password_textbox.Text = "";
+            }
+        }
+
+
+
     }
 }
